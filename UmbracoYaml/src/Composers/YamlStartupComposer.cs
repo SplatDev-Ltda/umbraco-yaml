4aa8e7b4f520bd3efb1153cd990e1d2b11286b4b
Developer <dev@example.com>
2026-03-28 19:05:08 -0300

    init: scaffold Umbraco 17 plugin project with dependencies

---
+++ UmbracoYaml/src/Composers/YamlStartupComposer.cs
@@ -0,0 +1,14 @@
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.Events;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UmbracoYaml.Composers
+{
+    public class YamlStartupComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            // Placeholder for dependency registration
+        }
+    }
+}
